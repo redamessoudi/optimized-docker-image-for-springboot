16784c16d93b76cd330edbf262edb3611930520d
REME-AlarmTILT <reda.messoudi@gmail.com>
2020-11-01 14:20:13 +0100

    Init the project

---
+++ src/main/java/com/redamessoudi/optimizedSBDimage/OptimizedSpringBootDockerImageApplication.java
@@ -0,0 +1,13 @@
+package com.redamessoudi.optimizedSBDimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizedSpringBootDockerImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizedSpringBootDockerImageApplication.class, args);
+	}
+
+}
